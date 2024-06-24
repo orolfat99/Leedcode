635843ff6e3c817d4089947f128c11f738c1b42c
orolfat99 <47807136+orolfat99@users.noreply.github.com>
2024-06-25 00:08:45 +1000

    Update 14_Longest Common Prefix.js

--- 14_Longest Common Prefix.js
+++ 14_Longest Common Prefix.js
@@ -1,10 +1,14 @@
 var longestCommonPrefix = function(strs) {
-    if (!strs.length) return "";
-    for (let i=0; i<= strs[0].length; i++){
-        for (let j=0; j<= strs.length; j++){
-            if (str[0][i] !== strs [j][i]){
-                return strs[0].slice(0,i);
-            }
-        }
+   if (strs.length == 0){
+    return "";
+   }
+
+   let prefix = strs[0];
+
+   for (let i=0; i<strs.length; i++){
+    while(strs[i].indexOf(prefix) !=0){
+        prefix = prefix.substring(0,prefix.length -1);
     }
+   }
+   return prefix;
 };
