c83a9a7c42e7e90684ed2bbe3c2c708769b2c51f
orolfat99 <47807136+orolfat99@users.noreply.github.com>
2024-06-24 22:09:07 +1000

    Create 14_Longest Common Prefix.js

---
+++ 14_Longest Common Prefix.js
@@ -0,0 +1,10 @@
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+    for (let i=0; i<= strs[0].length; i++){
+        for (let j=0; j<= strs.length; j++){
+            if (str[0][i] !== strs [j][i]){
+                return strs[0].slice(0,i);
+            }
+        }
+    }
+};
